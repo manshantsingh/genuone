1f507f4c950264f6e8021abc227b12f070963bfb
Manshant Singh Kohli <me@manshantsingh.com>
2018-01-14 13:49:29 -0800

    fixed bug plus less weighted neutral

--- backend/automate.py
+++ backend/automate.py
@@ -3,46 +3,67 @@ import websockets
 from subprocess import run
 from subprocess import PIPE
 from time import sleep
+from time import time
 import numpy as np
 import pandas as pd
 from ast import literal_eval
 import json
 
 async def hello(websocket, path):
-  run(['adb', 'shell', 'am', 'start', '-a', 'android.media.action.STILL_IMAGE_CAMERA'])
-  sleep(2)
-  columns = ['anger', 'contempt', 'disgust', 'fear', 'happiness', 'neutral', 'sadness', 'surprise']
-  data = []
-  last_n=[]
-  N=5
-  while True:
-    sleep(1)
-    run(['adb', 'shell' ,'input', 'keyevent', '27'])
+	run(['adb', 'shell', 'am', 'start', '-a', 'android.media.action.STILL_IMAGE_CAMERA'])
+	sleep(2)
+	columns = ['anger', 'contempt', 'disgust', 'fear', 'happiness', 'neutral', 'sadness', 'surprise']
+	data = []
+	last_n=[]
+	N=1
+	while True:
+		start = time()
+		run(['adb', 'shell' ,'input', 'keyevent', '27'])
 
-    sleep(1)
-    imgName=str(run(['adb', 'shell', 'ls', '/storage/emulated/0/DCIM/Camera/', '-t', '|', 'head', '-1'], stdout=PIPE).stdout)[2:-5]
+		imgName=str(run(['adb', 'shell', 'ls', '/storage/emulated/0/DCIM/Camera/', '-t', '|', 'head', '-1'], stdout=PIPE).stdout)[2:-5]
 
-    sleep(1)
-    run(['adb', 'pull', '/storage/emulated/0/DCIM/Camera/' + imgName])
+		run(['adb', 'pull', '/storage/emulated/0/DCIM/Camera/' + imgName])
 
-    sleep(1)
-    out=str(run(['node', 'index.js', imgName], stdout=PIPE).stdout)[2:-3]
-    print(out)
-    if out == "nothing":
-      continue
-    arr=[x.split(' ') for x in out.split('\\n')]
-    d=pd.DataFrame(arr, columns=columns, dtype=np.float).mean() * 100
-    data.append(d)
-    last_n.append(d)
-    if len(last_n) > N:
-      last_n.pop(0)
-    mean = pd.DataFrame(last_n).mean()
-    dic = {'last': literal_eval(mean.to_json()), 'winning': list(mean.sort_values(0, ascending=False).index[0:3]), 'type': 'emotions_data'}
-    await websocket.send(json.dumps(dic))
+		out=str(run(['node', 'index.js', imgName], stdout=PIPE).stdout)[2:-3]
+		print(out)
+		if out == "nothing" or out[0]=='{':
+			continue
+		arr=[x.split(' ') for x in out.split('\\n')]
+		fail=False
+		for x in arr:
+			if len(x) != 8:
+				fail=True
+				break
+		if fail:
+			print("columns mismatch will happen. Will ignore for safety")
+			print(arr)
+
+		df=pd.DataFrame(arr, columns=columns, dtype=np.float)
+		df['neutral'] = df.neutral*0.5
+		sums = df.sum(1)
+		df=df.div(sums, axis=0)
+		print(df.sum(1))
+		d=df.mean() * 100
+		data.append(d)
+		last_n.append(d)
+		if len(last_n) > N:
+			last_n.pop(0)
+
+		dic = {
+			'last': literal_eval(pd.DataFrame(last_n).mean().to_json()),
+			'winning': list(pd.DataFrame(data).mean().sort_values(0, ascending=False).index[0:3]),
+			'type': 'emotions_data'
+		}
+		await websocket.send(json.dumps(dic))
+		delta = time() - start
+		if delta < 5:
+			delta = 5-delta
+			print('spared '+str(delta)+' seconds')
+			sleep(delta)
+		else:
+			print('behind by '+str(delta-5)+' seconds')
 
 start_server = websockets.serve(hello, '0.0.0.0', 5000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
-
-
--- frontend/index.js
+++ frontend/index.js
@@ -42,6 +42,14 @@ function getHoursMinuteFormat(time) {
   return ret;
 }
 
+function barChart(){
+    $('.bar-chart').find('.item-progress').each(function(){
+      var itemProgress = $(this),
+        itemProgressWidth = $(this).parent().width() * ($(this).data('percent') / 100);
+      itemProgress.css('width', itemProgressWidth);
+    });
+  };
+  
 function manageEmotionsData(data) {
   var meanEmotions = data["last"];
   var barItems = "";
@@ -74,13 +82,7 @@ function manageEmotionsData(data) {
 
 $(document).ready(function(){
 
-  function barChart(){
-    $('.bar-chart').find('.item-progress').each(function(){
-      var itemProgress = $(this),
-        itemProgressWidth = $(this).parent().width() * ($(this).data('percent') / 100);
-      itemProgress.css('width', itemProgressWidth);
-    });
-  };
+  
 
   $(".app .student-questions #hand-raised-question-alert").click(function() {
     $(this).hide();
